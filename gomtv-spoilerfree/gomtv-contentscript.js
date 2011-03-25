36d6925fc6b4ba6819dc0b499d915fdc37768937
Marc Brinkmann <git@marcbrinkmann.de>
2011-03-25 16:11:36 +0100

    Improved spoiler-free code to allow revealing.

--- gomtv-spoilerfree/gomtv-contentscript.js
+++ gomtv-spoilerfree/gomtv-contentscript.js
@@ -1,17 +1,33 @@
+$.fn.showOrSpoiler = function(msg, hide) {
+	if (! hide) {
+		// do nothing, element will appear
+		console.log('showing',msg,$(this));
+	} else {
+		// insert spoilerific placeholder!
+		console.log('spoiler-protecting',msg,$(this));
+		// create new element to replace this one with
+		var placeholder = $('<div class="gomtv-spoilerfree-placeholder">' + msg + ' hidden to avoid spoilers. Click to reveal.</div>');
+		placeholder.data('replacedElement', $(this));
+		$(this).replaceWith(placeholder);
+
+		placeholder.replacedElement = $(this);
+		$(placeholder).click(function() {
+			console.log('replacing with',$(this).data('replacedElement'));
+			$(this).replaceWith($(this).data('replacedElement'));
+		})
+	}
+
+	// make visible
+	return $(this).css('visibility', 'visible');
+}
+
 function apply_to_page(options) {
 	console.log('running...')
-	if (! options['hide-recent-videos']) {
-		$(".box_vod").each(function(n) {
-			$(this).css('visibility', 'visible'); });
-		$("#ch_vod").css('visibility', 'visible');
-		$("#sugShowArea").css('visibility', 'visible');
-	}
-	if (! options['hide-social-networks']) {
-		$("#SNS_Update").css('visibility', 'visible');
-	}
-	if (! options['hide-comments']) {
-		$("#replyFrame").css('visibility', 'visible');
-	}
+	$(".box_vod").each(function(n) { $(this).showOrSpoiler("Recent video/photo list", options['hide-recent-videos']) })
+	$("#ch_vod").showOrSpoiler("Video list", options['hide-recent-videos']);
+	$("#sugShowArea").showOrSpoiler("Suggested video list", options['hide-recent-videos']);
+	$("#SNS_Update").showOrSpoiler("Social networks", options['hide-social-networks']);
+	$("#replyFrame").showOrSpoiler("Comments", options['hide-comments']);
 }
 
 $(document).ready(function() {
