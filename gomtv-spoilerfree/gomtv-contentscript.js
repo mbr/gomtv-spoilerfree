117d8dfcbd2108fa051a860829abd27de526f732
Marc Brinkmann <git@marcbrinkmann.de>
2011-03-25 16:25:24 +0100

    Made showOrSpoiler() work on collections.

--- gomtv-spoilerfree/gomtv-contentscript.js
+++ gomtv-spoilerfree/gomtv-contentscript.js
@@ -1,34 +1,36 @@
 $.fn.showOrSpoiler = function(msg, hide) {
-	if (! hide) {
-		// do nothing, element will appear
-		console.log('showing',msg,$(this));
-	} else {
-		// insert spoilerific placeholder!
-		console.log('spoiler-protecting',msg,$(this));
-		// create new element to replace this one with
-		var placeholder = $('<div class="gomtv-spoilerfree-placeholder">' + msg + ' hidden to avoid spoilers. Click to reveal.</div>');
-		placeholder.data('replacedElement', $(this));
-		$(this).replaceWith(placeholder);
+	return $(this).each(function() {
+		if (! hide) {
+			// do nothing, element will appear
+			console.log('showing',msg,$(this));
+		} else {
+			// insert spoilerific placeholder!
+			console.log('spoiler-protecting',msg,$(this));
+			// create new element to replace this one with
+			var placeholder = $('<div class="gomtv-spoilerfree-placeholder">' + msg + ' hidden to avoid spoilers. Click to reveal.</div>');
+			placeholder.data('replacedElement', $(this));
+			$(this).replaceWith(placeholder);
 
-		placeholder.replacedElement = $(this);
-		$(placeholder).click(function() {
-			console.log('replacing with',$(this).data('replacedElement'));
-			$(this).replaceWith($(this).data('replacedElement'));
-		})
-	}
+			placeholder.replacedElement = $(this);
+			$(placeholder).click(function() {
+				console.log('replacing with',$(this).data('replacedElement'));
+				$(this).replaceWith($(this).data('replacedElement'));
+			})
+		}
 
-	// make visible
-	return $(this).css('visibility', 'visible');
+		// make visible
+		return $(this).css('visibility', 'visible');
+	});
 }
 
 function apply_to_page(options) {
 	console.log('running...')
-	$(".box_vod").each(function(n) { $(this).showOrSpoiler("Recent video/photo list", options['hide-recent-videos']) })
+	$(".box_vod").showOrSpoiler("Recent video/photo list", options['hide-recent-videos']);
 	$("#ch_vod").showOrSpoiler("Video list", options['hide-recent-videos']);
 	$("#sugShowArea").showOrSpoiler("Suggested video list", options['hide-recent-videos']);
 	$("#SNS_Update").showOrSpoiler("Social networks", options['hide-social-networks']);
 	$("#replyFrame").showOrSpoiler("Comments", options['hide-comments']);
-	$(".slist_off").each(function(n) { $(this).showOrSpoiler("Match", options['hide-schedule']); });
+	$(".slist_off").showOrSpoiler("Match", options['hide-schedule']);
 }
 
 $(document).ready(function() {
