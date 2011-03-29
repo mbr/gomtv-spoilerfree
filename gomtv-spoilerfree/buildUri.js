5327aedd179caeae51190bf4708a094bb1bac286
Marc Brinkmann <git@marcbrinkmann.de>
2011-03-29 10:27:24 +0200

    Removed accidentally committed buildUri.js.

--- gomtv-spoilerfree/buildUri.js
+++
@@ -1,8 +0,0 @@
-// buildUri
-// written by me!
-
-function buildUri(d) {
-	keys = ["source", "protocol", "authority", "userInfo", "user", "password", "host", "port", "relative", "path", "directory" ,"file","query","anchor"],
-
-	return 
-}
